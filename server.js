bd121b6d2b196f71a0c992d7367ab40cc9453bc5
Hector Garcia <kotikkid@gmail.com>
2020-08-25 21:40:36 -0700

    Express and routes set up

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const express = require('express'); 
 
+const connectDB = require('./config/db');
+const { connect } = require('mongoose');
+
 const app = express();
 
+// Connect Database
+connectDB();
+
 app.get('/', (req, res) => res.send('API Running'));
+
+//Define routes
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/post', require('./routes/api/post'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/users', require('./routes/api/users'));
 
 const PORT = process.env.PORT || 5000;
 
